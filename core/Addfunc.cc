67216ee840e17e869931c625b388ba9718e0755b
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-09-04 14:52:39 +0200

    slightly simplified expression.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- core/Addfunc.cc
+++ core/Addfunc.cc
@@ -2988,17 +2988,17 @@ CHARSTRING get_stringencoding(const OCTETSTRING& encoded_value)
   if (!encoded_value.lengthof()) return CHARSTRING("<unknown>");
   unsigned int i, j, length = encoded_value.lengthof();
   const unsigned char* strptr = (const unsigned char*)encoded_value;
-  for (i = 0, j = 0; UTF8_BOM[i++] == strptr[j++] && i < sizeof (UTF8_BOM););
+  for (i = 0, j = 0; UTF8_BOM[i] == strptr[j] && i < sizeof (UTF8_BOM); i++, j++);
   if (i == sizeof (UTF8_BOM) && sizeof(UTF8_BOM) <= length) return "UTF-8";
   //UTF-32 shall be tested before UTF-16 !!!
-  for (i = 0, j = 0; UTF32BE_BOM[i++] == strptr[j++] && i < sizeof (UTF32BE_BOM););
+  for (i = 0, j = 0; UTF32BE_BOM[i] == strptr[j] && i < sizeof (UTF32BE_BOM); i++, j++);
   if (i == sizeof (UTF32BE_BOM) && sizeof (UTF32BE_BOM) <= length ) return "UTF-32BE";
-  for (i = 0, j = 0; UTF32LE_BOM[i++] == strptr[j++] && i < sizeof (UTF32LE_BOM););
+  for (i = 0, j = 0; UTF32LE_BOM[i] == strptr[j] && i < sizeof (UTF32LE_BOM); i++, j++);
   if (i == sizeof (UTF32LE_BOM) && sizeof (UTF32LE_BOM) <= length) return "UTF-32LE";
   //UTF-32 shall be tested before UTF-16 !!!
-  for (i = 0, j = 0; UTF16BE_BOM[i++] == strptr[j++] && i < sizeof (UTF16BE_BOM););
+  for (i = 0, j = 0; UTF16BE_BOM[i] == strptr[j] && i < sizeof (UTF16BE_BOM);  i++, j++);
   if (i == sizeof (UTF16BE_BOM) && sizeof (UTF16BE_BOM) <= length) return "UTF-16BE";
-  for (i = 0, j = 0; UTF16LE_BOM[i++] == strptr[j++] && i < sizeof (UTF16LE_BOM););
+  for (i = 0, j = 0; UTF16LE_BOM[i] == strptr[j] && i < sizeof (UTF16LE_BOM); i++, j++);
   if (i == sizeof (UTF16LE_BOM) && sizeof (UTF16LE_BOM) <= length) return "UTF-16LE";
   if (is_ascii (encoded_value) == CharCoding::ASCII) {
     return "ASCII";
